4c5d11bec50b8a7baf3f0adb41b1e3a5b38b67e6
brunocoio <brunocoio@gmail.com>
2024-03-20 21:42:29 -0300

    update: aula 65 fim do projeto

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -3,12 +3,25 @@ const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
+const simbolos = [
+    '.', '?', '-', ',', '"', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')'
+]
+
+
+
 fn.lerDiretorio(caminho)
     .then(fn.elementosTerminadosCom('.srt'))
     .then(fn.lerArquivos)
-    .then(conteudos => conteudos.join('\n'))
-    .then(TodoConteudo => TodoConteudo.split('\n'))
+    .then(fn.mesclarElementos)
+    .then(fn.separarTextoPor('\n'))
     .then(fn.removerElementosSeVazio)
     .then(fn.removerElementosSeIncluir('-->'))
     .then(fn.removerElementosSeApenasNumero)
+    .then(fn.removerSimbolos(simbolos))
+    .then(fn.mesclarElementos)
+    .then(fn.separarTextoPor(' '))
+    .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosSeApenasNumero)
+    .then(fn.agruparElementos)
+    .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
     .then(console.log)
